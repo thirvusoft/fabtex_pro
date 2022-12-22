67646cd0deac9f3a50393279e682bc027a0457a5
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-12-22 12:32:33 +0000

    feat: small changes updation

--- fabtex_pro/utils/after_install.py
+++ fabtex_pro/utils/after_install.py
@@ -1,6 +1,8 @@
 from fabtex_pro.utils.selling.sales_invoice import sales_invoice_customization
 from fabtex_pro.utils.company.company import company_customization
+from fabtex_pro.utils.selling.customer import customer_customization
 
 def after_install():
     company_customization()
+    customer_customization()
     sales_invoice_customization()
